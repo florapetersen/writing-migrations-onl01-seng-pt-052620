c3488a5147df2c4ebff30e75427048b82142604f
Flora Petersen <floramarina@comcast.net>
2020-08-14 20:48:44 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,12 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT
-      )
-      SQL
-  end
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  )
+  SQL
 
   def change
     create_table :students do |t|
