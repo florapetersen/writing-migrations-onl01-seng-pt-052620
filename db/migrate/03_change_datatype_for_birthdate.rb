b7b4babeebf0f1ed0d0021a1809954e43d521d8c
Flora Petersen <floramarina@comcast.net>
2020-08-14 20:59:55 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthday < ActiveRecord::Migration[5.1]
   def change_column
-    change_column(:students, :birthdate, :datetime)
+    change_column :students, :birthdate, :datetime
   end
 end
